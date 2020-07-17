4034c4fed9af5de63a21c1a9ddc7927dfe1f7566
Wilgnne <wilgnne.kba@gmail.com>
2020-07-17 11:58:16 -0300

    CPU call in main

--- src/main.c
+++ src/main.c
@@ -1,19 +1,7 @@
-#include <stdbool.h>
-
 #include <cpu.h>
-
-bool running = true;
 
 int main(int argc, char const *argv[])
 {
-    int instr;
-
-    while (running)
-    {
-        instr = fetch();
-        if (instr == HLT)
-            running = false;
-    }
-
+    cpu();
     return 0;
 }
